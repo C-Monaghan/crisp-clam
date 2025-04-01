03cf174c9b440eb3e8841130d89ce2b4fb63a88f
C-Monaghan <cormacmonaghan@tutanota.com>
2025-04-01 13:03:53 +0100

    Update :bug:

--- R/conversion.R
+++
@@ -1,8 +0,0 @@
-
-data <- readxl::read_xlsx(here::here("data/lw_data_full.xlsx"))
-
-
-
-write.csv(x = data, file = here::here("data/data.csv"))
-saveRDS(object = data, file = here::here("data/data.rds"))
-haven::write_dta(data = data, path = here::here("data/data.dta"))
